d7cf525e12a4016c24b2b3a5c4564bc080f14133
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-09 13:40:17 -0600

    add basic frontend files

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <div id="main">
+      Rendering
+    </div>
+  )
+}
+
+export default App;
