5197669a38287fc5145bb1d63360bc4148b17dc9
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-10 13:50:09 -0600

    add basic login, register forms

--- client/containers/Login.jsx
+++ client/containers/Login.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="mid">
-      Login
+    <div className="mid col auth">
+      <h2>Welcome back!</h2>
+      <form className="col">
+        <label htmlFor="username">Username</label>
+        <input type="text" required/>
+        <label htmlFor="username">Password</label>
+        <input type="text" required/>
+      </form>
+      <button type="submit">Login</button>
     </div>
   )
 }
--- client/containers/Register.jsx
+++ client/containers/Register.jsx
@@ -3,8 +3,19 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div className="mid">
-      Register
+    <div className="mid col auth">
+      <h2>Welcome to FittR!</h2>
+      <form className="col auth-form">
+        <label htmlFor="username">Email</label>
+        <input type="email" name="username" id="username" />
+        <label htmlFor="name">Display Name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <label htmlFor="confirm">Confirm Password</label>
+        <input type="password" name="confirm" id="confirm" />
+      </form>
+      <button type="submit">Submit</button>
     </div>
   )
 }
