52ab205f86beefa565cfc91f2c2db3a830e7f326
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-10 11:45:01 -0600

    add basic routes

--- server/routes/weightRouter.js
+++ server/routes/weightRouter.js
@@ -2,7 +2,45 @@ const express = require('express');
 const router = express.Router();
 const weightController = require('../controllers/weightController');
 
+/**
+ * Post weight 
+ * req.body - weight (number), user_id (number)
+ * middleware - 
+ * res.locals - n/a
+ */
+ router.post('/', (req, res) => {
+  return res.send(res.locals);
+});
 
+/**
+ * Get weights route
+ * req.body - user_id (number), year (number)
+ * middleware -
+ * res.locals - array of objs with keys weight (number), date (date/str), _id (number)
+ */
+router.get('/', (req, res) => {
+  return res.send(res.locals);
+});
+
+/**
+ * Delete weight route
+ * req.body - user_id (number), [ weight_ids (numbers) ]
+ * middleware -
+ * res.locals - n/a
+ */
+ router.delete('/', (req, res) => {
+  return res.send(res.locals);
+});
+
+/**
+ * Update weight route
+ * req.body - user_id (number), weight_id (number), weight (number)
+ * middleware -
+ * res.locals - 
+ */
+ router.patch('/', (req, res) => {
+  return res.send(res.locals);
+});
 
 
 module.exports = router;
