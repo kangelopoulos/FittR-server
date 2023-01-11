234b55e96066c75d2b9ca2d1c7af82ba9c178ad8
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-11 16:29:37 -0600

    attempt to vary origin

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://fittr.onrender.com/');
+  res.setHeader('Vary', 'Origin');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
