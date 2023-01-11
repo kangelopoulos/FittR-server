d045bd56815cbe3f22f89df55779ed9c4b729a92
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-11 16:37:58 -0600

    remove wildcard

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://fittr.onrender.com*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://fittr.onrender.com');
   res.setHeader('Vary', 'Origin');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
