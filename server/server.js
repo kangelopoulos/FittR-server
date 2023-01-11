113161f785e96a49f0cc1458264535e6f9c2c51f
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-11 16:25:39 -0600

    fix cors uri

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://fittr.onrender.com/*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://fittr.onrender.com/');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
